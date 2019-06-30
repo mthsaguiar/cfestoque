55992f9e04cb29ec0adfa1b1b9571d34f539bcee
Matheus Aguiar <mths.aguiarr@gmail.com>
2019-06-30 18:19:44 -0300

    Primeiros componentes e pagina de move estoque criados.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react'
+import Header from './Components/Header'
+import NavBar from './Components/NavBar'
+import MovePage from './Pages/MovePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class extends Component {
+render (){
+return <Fragment>
+<Header/>
+
+<NavBar/>
+
+<MovePage/>
+
+</Fragment>
 }
-
-export default App;
+}
