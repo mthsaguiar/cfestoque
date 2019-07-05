9874621e4c701ac35b46213f0ffc94e7c5ba70fc
Matheus Aguiar <mths.aguiarr@gmail.com>
2019-07-04 21:51:43 -0300

    First components and page

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import Icon from '@material-ui/core/Icon'
+import ListItemText from '@material-ui/core/ListItemText'
+import addProdButton from '../assets/baseline-add_box-24px.svg'
+import inOutButton from '../assets/baseline-swap_vert-24px.svg'
+import sideBar from '../images/sideBar.jpg'
+
+const navBarWidth = 240;
+
+const styles = makeStyles (theme =>({
+staticBox: {
+    width: navBarWidth,
+    height: 64,
+},
+sideBar: {
+width: '100%',
+maxWidth: navBarWidth,
+height: '100vh'
+},
+menuList: {
+width: '100%',
+maxWidth: navBarWidth,
+justifyContent: 'center',
+alignItems: 'flex-end',
+},
+icon: {
+    margin: theme.spacing(2),
+},
+}));
+
+export default function navBar() {
+    
+const ImageSideBar = sideBar;
+const classes = styles();
+
+return(
+<navBar >
+<div className={classes.sideBar} style={{backgroundImage: `url(${ImageSideBar})` }}>
+    <div className={classes.staticBox}/>
+        <Divider />
+
+            <div className={classes.menuList} >
+                <List component="nav" aria-label="Menu de navegação">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <Icon className={classes.icon}>
+                                <img src={addProdButton} alt="addProduto"/>
+                            </Icon>
+                        </ListItemIcon>
+                        <ListItemText primary="PRODUTO"/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <Icon className={classes.icon}>
+                                <img src={inOutButton} alt="inOutButton"/>
+                            </Icon>
+                        </ListItemIcon>
+                        <ListItemText primary="MOVIMENTAÇÃO"/>
+                    </ListItem>
+                </List>
+            </div>
+</div>
+</navBar>
+);
+}
